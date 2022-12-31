705305a9b3fb410a4c741d500c9400b8f8b05aed
indrajit-rookie <indra96963@gmail.com>
2023-01-01 02:00:43 +0530

    Updated deployment issue static-public thing

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
+
+app.use(express.static(__dirname + "/public/"));
 
 
 
